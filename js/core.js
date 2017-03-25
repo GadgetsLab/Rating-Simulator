1f33d12c716bc9bb331948f9d484a2893d4b64b9
daesca <reickchozo@gmail.com>
2017-03-25 18:23:16 -0500

    Adaptando la lógica a los cambios de la vista

--- js/core.js
+++ js/core.js
@@ -1,4 +1,4 @@
-/*
+804-5100
 var form = document.getElementById('form');
 //xmlhttp = new XMLHttpRequest();
 form.addEventListener("submit", getResponse, false);
@@ -8,28 +8,33 @@ function getResponse(e)
     e.preventDefault();
     totalAnswers = document.getElementsByClassName('question');
     var answer= [];
-    var b = [];
-    for(i = 0; i < totalAnswers.length; i++){
-            var a = totalAnswers[i].children[1].value;
-            b[i] = totalAnswers[i].children[1].value.toLowerCase();
-            answer[i] = a.toLowerCase().replace(',', '').split(' ');
+    var c = [];
+    for(var i = 0; i < totalAnswers.length; i++){
+            var a = totalAnswers[i].children[1]; //value
+            var b = a.children[0].value.toLowerCase();
+            answer[i] = b.replace(',', '').split(' ');
+            //c[i] = totalAnswers[i].children[1].value.toLowerCase();
         }
-    for(i = 0; i < answer.length; i++)
+    console.log(answer.length);
+    
+
+    for(var i = 0; i < answer.length; i++)
         {
-            for(x = 0; x < answer[i].length; x++ ){
+            for(var x = 0; x < answer[i].length; x++ ){
                 
                 if(respuestas[i].indexOf(answer[i][x]) >= 0){
                 cont++;
-                c = b[i];
-                b[i] = c.replace(answer[i][x], "<span class='red'>" + answer[i][x] + "</span>");
+                var d = c[i];
+                c[i] = d.replace(answer[i][x], "<span class='red'>" + answer[i][x] + "</span>");
                 }
                 palabras++;
             }
         }
     console.log(cont + " - " + palabras + " - " + Math.round((cont/palabras) * 100));
-    console.log(b);
+/*
+*/
 }
-
+/*
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myArr = this.responseText;
